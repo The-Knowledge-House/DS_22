a5478e36ee0ea69a907114fe1018a70af0495d23
F-said <f.saidmuratov@yahoo.com>
2022-09-20 11:30:23 -0400

    Moved lab to week 4

--- Phase1/week4/lab/palnumber.py
+++ Phase1/week4/lab/palnumber.py
@@ -1,10 +1,10 @@
 import sys
 
-def palindrome():
+def main():
     """Insert code here"""
     pass
 
 if __name__=="__main__":
     # take in the number
     number = sys.argv[1]
-    palindrome(number)
+    main(number)
