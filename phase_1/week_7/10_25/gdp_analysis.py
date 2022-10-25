c4c65b650f45c5354a6e9e56552442d08cd61cdd
F-said <f.saidmuratov@yahoo.com>
2022-10-25 16:26:12 -0400

    Added package

--- phase_1/week_7/10_25/gdp_analysis.py
+++ phase_1/week_7/10_25/gdp_analysis.py
@@ -1,3 +1,5 @@
+import csv
+
 def get_highest_gdp(year):
     return
 
--- phase_1/week_7/Lab/skeleton.py
+++ phase_1/week_7/Lab/skeleton.py
@@ -1,3 +1,29 @@
+import csv
+
 # code to get years from 1997 to 2020
 years = [str(year) for year in range(1997, 2021)]
 
+def get_highest_gdp(year):
+    pass
+
+def get_lowest_gdp(year):
+    pass
+
+with open() as infile:
+    # load in data as DictReader
+
+    # pass data to get_highest_gdp and print output
+
+    # pass data to get_lowest_gdp and print output
+
+    # remove this pass
+    pass
+
+with open("highest_gdp.csv", "w") as outfile:
+    # open dict
+    pass
+
+
+# CHALLENGE (optional)
+def get_percent_change(state, year1, year2):
+    pass
