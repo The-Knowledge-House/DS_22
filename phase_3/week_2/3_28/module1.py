5bc073b9a3b50669267e9a770302a79e9e4e7725
F-said <26397102+F-said@users.noreply.github.com>
2023-03-28 13:47:14 -0400

    added phase 3 material

---
+++ phase_3/week_2/3_28/module1.py
@@ -0,0 +1,2 @@
+secret1 = "Everything in Python is an object"
+secret2 = "The Hudson River is an estuary"
---
+++ phase_3/week_2/3_28/module2.py
@@ -0,0 +1,3 @@
+def secret_func():
+    x = input("Input a positive number and convert to binary")
+    print(bin(int(x)))
