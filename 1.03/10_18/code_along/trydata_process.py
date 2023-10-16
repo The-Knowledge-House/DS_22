3a7e2e40f9611490efbc444caac21428b4f565ce
F-said <26397102+F-said@users.noreply.github.com>
2023-10-16 15:25:50 -0400

    Added all documents

---
+++ 1.03/10_18/code_along/trydata_process.py
@@ -0,0 +1,12 @@
+data = [130, 500, 1000, "404", 530, 200, 150_000, 100_000,
+        "404", 80_000, 30_000, 2000]
+
+retention_rate = 0.03
+
+# TODO: run this code
+for pop in data:
+    try:
+        print(f"we retained {retention_rate * pop} users")
+    except TypeError:
+        print("Improper type, skipping")
+        continue
